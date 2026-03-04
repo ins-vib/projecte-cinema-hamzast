3accb52beb200fcb7269f18f4ee74800513e1845
«hamza» <hamzabenelallid@gmail.comm>
2026-03-04 10:18:55 +0100

    nivel classe alfin

--- src/main/java/com/daw/CinemaDaw/controller/HomeController.java
+++ src/main/java/com/daw/CinemaDaw/controller/HomeController.java
@@ -1,15 +1,9 @@
 package com.daw.CinemaDaw.controller;
 
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import com.daw.CinemaDaw.domain.cinema.Cinema;
 import com.daw.CinemaDaw.repository.CinemaRepository;
 
 @Controller
@@ -25,26 +19,7 @@ public class HomeController {
     public String home(){
         return "home";
     }
-
-     @GetMapping("/cinemes")
-    public String cinemes(Model model){
-
-        List<Cinema> cinemes = cinemaRepository.findAll();
-        model.addAttribute("llista", cinemes);
-        return "cinemes";
-    }
     
-    //detall cinema
-    @GetMapping("/cinema/{id}")
-    public String detall(@PathVariable Long id, Model model){
-        Optional<Cinema> optional = cinemaRepository.findById(id);
-        if(optional.isPresent()){
-            Cinema cinema = optional.get();
-            model.addAttribute("cinema", cinema);
-            return "cinemes-detall";
-        }
-        return "redirect:/";
-       
-
-    }
 }
+
+
---
+++ src/main/java/com/daw/CinemaDaw/controller/cinemaController.java
@@ -0,0 +1,98 @@
+package com.daw.CinemaDaw.controller;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.daw.CinemaDaw.domain.cinema.Cinema;
+import com.daw.CinemaDaw.repository.CinemaRepository;
+
+@Controller
+public class cinemaController {
+
+    private CinemaRepository cinemaRepository;
+
+    public cinemaController(CinemaRepository cinemaRepository){
+        this.cinemaRepository=cinemaRepository;
+    }
+
+   
+
+     @GetMapping("/cinemes")
+    public String cinemes(Model model){
+
+        List<Cinema> cinemes = cinemaRepository.findAll();
+        model.addAttribute("llista", cinemes);
+        return "cinemes";
+    }
+    
+    //detall cinema
+    @GetMapping("/cinema/{id}")
+    public String detall(@PathVariable Long id, Model model){
+        Optional<Cinema> optional = cinemaRepository.findById(id);
+        if(optional.isPresent()){
+            Cinema cinema = optional.get();
+            model.addAttribute("cinema", cinema);
+            return "cinemes-detall";
+        }
+        return "redirect:/";
+       
+
+    }
+
+
+    //crear cinema
+    @GetMapping("/cinema/create")
+    public String create(Model model){
+        Cinema cinema = new Cinema();
+        model.addAttribute("cinema", cinema);
+        return "create-cinema";
+    }
+    @PostMapping("/cinema/create")
+    public String create(Cinema cinema){
+        cinemaRepository.save(cinema);
+        return "redirect:/cinemes";
+    }
+
+//borrar cinema
+    @GetMapping("/cinema/delete/{id}")
+    public String delete(@PathVariable Long id, Model model){
+        Optional<Cinema> optional = cinemaRepository.findById(id);
+        if(optional.isPresent()){
+            Cinema cinema = optional.get();
+            cinemaRepository.delete(cinema);
+        }
+        return "redirect:/cinemes";
+    }
+
+//editar cinema
+    @GetMapping("/cinema/edit/{id}")
+    public String edit(@PathVariable Long id, Model model){
+        Optional<Cinema> optional = cinemaRepository.findById(id);
+        if(optional.isPresent()){
+            Cinema cinema = optional.get();
+            model.addAttribute("cinema", cinema);
+            return "edit-cinema";
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping("/cinema/edit")
+    public String edit(Cinema cinema){
+        cinemaRepository.save(cinema);
+        return "redirect:/cinemes";
+    }
+
+
+
+
+    
+}
+
+
